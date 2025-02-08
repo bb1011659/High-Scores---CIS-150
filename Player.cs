1e6bf591e3dedf9762244ae9e375cf98927af682
bb1011659 <Bb1011659@otc.edu>
2025-02-08 13:23:23 -0600

    High Scores

---
+++ Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1HighScores
+{
+    internal class Player
+    {
+        private string initials;
+        private int score;
+        public string Initials { get; set; }
+        public int Score { get; set; }
+
+        public Player(string? initials, int score)
+        {
+            Initials = initials;
+            Score = score;
+        }
+
+        public void HighScore()
+        {
+            if(Score <= 0)
+            {
+                Console.WriteLine("Invalid Score");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations {Initials}! Your score is {Score}!\nWell done!");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace Mod1HighScores
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Player player1 = new Player("BAB", 100000);
+
+            player1.HighScore();
+
+            
+        }
+    }
+}
